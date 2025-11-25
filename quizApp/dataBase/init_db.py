406e401f5abff67709c04b692032054bdcab33f5
3omarsaid <omarelking288@gmail.com>
2025-11-25 19:31:47 +0200

    initialization and buildig db

---
+++ quizApp/dataBase/init_db.py
@@ -0,0 +1,58 @@
+from .repository import dbManager
+dbIns = dbManager()
+def createTables():
+    conn = dbIns.connect()
+    curs = conn.cursor()
+    #Users
+    curs.execute("""create table if not exists users(
+                Uid integer primary key autoincrement,
+                userName text not null,
+                password text not null,
+                role text not null)""")
+    conn.commit()
+    #Exams
+    curs.execute("""create table if not exists exams(
+                ExID integer primary key autoincrement,
+                TchID integer,
+                title text,
+                timerType text,
+                time_S int,
+                foreign key (TchID) references users(Uid))""")
+    conn.commit()
+    #Qustions
+    curs.execute("""create table if not exists qustions(
+                QusID integer primary key autoincrement,
+                ExID integer,
+                header text,
+                foreign key (ExID) references exams(ExID))""")
+    conn.commit()
+    #choices
+    curs.execute("""create table if not exists choices(
+                ChID integer primary key autoincrement,
+                QusID integer,
+                text text,
+                correct bool,
+                foreign key (QusID) references qustions(QusID))""")
+    conn.commit()
+    #attempts
+    curs.execute("""create table if not exists attempts(
+                AttID integer primary key autoincrement,
+                ExID integer,
+                StuID integer,
+                TchID integer,
+                start_at datetime default currunt_timestamp,
+                score integer,
+                duration_S float,
+                foreign key (ExID) references exams(ExID),
+                foreign key (StuID) references users(Uid))""")
+    conn.commit()
+    #Answers
+    curs.execute("""create table if not exists answers(
+                AnsID integer primary key autoincrement,
+                QusID integer,
+                AttID integer,
+                ChID integer,
+                correctnes integer,
+                foreign key (QusID) references quistions(QusID),
+                foreign key (AttID) references attempts(AttID))""")
+    conn.commit()
---
+++ quizApp/dataBase/repository.py
@@ -0,0 +1,48 @@
+import sqlite3
+class dbManager:
+    def __init__(self,path = "quiz.db"):
+        self.path = path
+    def connect(self):
+        conn = sqlite3.connect(self.path)
+        conn.execute("pragma forign_keys = on;")
+        return conn
+    def insert(self,table,row:dict):
+        conn = self.connect()
+        curs = conn.cursor()
+        keys = ','.join(row.keys())
+        placeholders = ','.join(['?']*len(row))
+        params = tuple(row.values())
+        curs.execute(f"""insert into {table}({keys})
+                    values({placeholders})""",params)
+        conn.commit()
+    def getAll(self,table):
+        conn = self.connect()
+        curs = conn.cursor()
+        return curs.execute(f"select * from {table}").fetchall()
+    def get(self,table,where : dict):
+        key = list(where.keys())[0]
+        val = list(where.values())[0]
+        conn = self.connect()
+        curs = conn.cursor()
+        return curs.execute(f"select * from {table} where {key} = ?",(val,)).fetchall()
+    def delete(self,table,where : dict):
+        key = list(where.keys())[0]
+        val = list(where.values())[0]
+        conn = self.connect()
+        curs = conn.cursor()
+        curs.execute(f"delete from {table} where {key} = ?",(val,))
+        conn.commit()
+    def update(self,table,row:dict,where:dict):
+        conn = self.connect()
+        curs = conn.cursor()
+        key = list(where.keys())[0]
+        keys = ' = ? ,'.join(row.keys())+' = ? '
+        params = tuple(list(row.values())+list(where.values()))
+        curs.execute(f"""update {table} set {keys} where {key} = ?""",params)
+        conn.commit()
+    def query(self,query,params:tuple):
+        conn = self.connect()
+        curs = conn.cursor()
+        curs.execute(query,params)
+        conn.commit()
+        return curs.fetchall()
