b18a0b7c4af33e3f7faabae97f5f0cc6e17b425d
3omarsaid <omarelking288@gmail.com>
2025-12-01 22:06:12 +0200

    edits on qustch

--- quizApp/pages/Qesfortec.py
+++ quizApp/pages/Qesfortec.py
@@ -7,37 +7,52 @@ db = init_db.dbIns
 title=st.text_input("title",placeholder="enter the title of the exam")
 timertype=st.radio("timeer type",["Question","exam"])
 timeinmin=st.number_input("time in minutes")
+if "count" not in st.session_state:
+    st.session_state.count=0
+if "exmid" not in st.session_state:
+    st.session_state.exmid = None
 if st.button("create exam"):
-    exmId=db.insert("exams",{"TchID":2,"title":title,"timerType":timertype,"time_s":timeinmin*60})
+    st.session_state.exmid =db.insert("exams",{"TchID":2,"title":title,"timerType":timertype,"time_s":timeinmin*60})
+    st.write(st.session_state.exmid)
+if st.session_state.exmid:
+    qus={}
+    choices = []
+    def creation(indx):
+        global qus
+        global choices
+        header=st.text_input("header",placeholder="enter your qestion ",key=f"header{indx}")
+        ch1=st.text_input("choose 1",placeholder="enter your choose ",key=f"ch1{indx}")
+        ch2=st.text_input("choose 2",placeholder="enter your choose ",key=f"ch2{indx}")
+        ch3=st.text_input("choose 3",placeholder="enter your choose ",key=f"ch3{indx}")
+        ch4=st.text_input("choose 4",placeholder="enter your choose ",key=f"ch4{indx}")
+        st.write("-------------------------------")
+        if i==st.session_state.count:
+            qus={"ExID":st.session_state.exmid,"header":header}
+            choices=[{"text":ch1,"correct":False},
+                        {"text":ch2,"correct":True},
+                        {"text":ch3,"correct":False},
+                        {"text":ch4,"correct":False},]
+    for i in range(st.session_state.count+1):
+        creation(i)
+    sub=st.button("submit")
+    add=st.button("Add anthor Qestion")
+    if add:
+        qusid = db.insert("qustions",qus)
+        for cho in choices:
+            cho["QusID"] = qusid
+        db.insertMany("choices",choices)
+        st.session_state.count+=1
+        st.rerun()
 
-if "questions" not in st.session_state:
-    st.session_state.questions=[]
-qus={}
-def creation(indx):
-    global qus
-    header=st.text_input("header",placeholder="enter your qestion ",key=f"header{indx}")
-    ch1=st.text_input("choose 1",placeholder="enter your choose ",key=f"ch1{indx}")
-    ch2=st.text_input("choose 2",placeholder="enter your choose ",key=f"ch2{indx}")
-    ch3=st.text_input("choose 3",placeholder="enter your choose ",key=f"ch3{indx}")
-    ch4=st.text_input("choose 4",placeholder="enter your choose ",key=f"ch4{indx}")
-    st.write("-------------------------------")
-    if i==len(st.session_state.questions):
-       qus={"ExID":exmId,"header":header}
-       choices={}
-for i in range(len(st.session_state.questions)+1):
-     creation(i)
-sub=st.button("submit")
-add=st.button("Add anthor Qestion")
-if add:
-   st.session_state.questions.append(qus)
-   st.rerun()
+    if sub:
+        qusid = db.insert("qustions",qus)
+        for cho in choices:
+            cho["QusID"] = qusid
+        db.insertMany("choices",choices)
+        st.session_state.clear()
+        st.switch_page("pages/exams.py")
 
-if sub:   
-    st.session_state.clear()
-    st.rerun()
 
-st.write(st.session_state.get("questions","no questions"))
-    
 
 
 
