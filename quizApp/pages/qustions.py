4ebd001fa231824130f77621b111671a9cf7325d
aishaahmedmosa1234-dev <aishaahmedmosa1234@gmail.com>
2025-12-04 23:32:46 +0200

    .

--- quizApp/pages/qustions.py
+++ quizApp/pages/qustions.py
@@ -5,14 +5,33 @@ import streamlit as st
 from quizApp.dataBase import init_db
 db = init_db.dbIns
 exid= st.session_state["exid"]
+tchid = 2
+stuid = 1
 # st.write(st.session_state.action)
 qustions = db.get("qustions",{"ExID":exid})
 if "index" not in st.session_state:
-    st.session_state.index = 0
+    st.session_state.index = 2
 index = st.session_state.index
-st.title(qustions[index][2])
-choices = db.get("choices",{"QusID":qustions[index][0]})
-st.write(choices)
-if st.button("next"):
-    st.session_state.index = index+1
-    st.rerun()
+if "score" not in st.session_state:
+    st.session_state.score = 0
+score = st.session_state.score
+if "attid" not in st.session_state:
+    attid=db.insert("attempts", {"ExID":exid , "StuID":stuid , "TchID":tchid , "score":0 , "duration_S":0})
+    st.session_state.attid = attid
+attid = st.session_state.attid
+if index == len(qustions):
+    att=db.get("attempts", {"AttID":attid})
+    st.write(att)
+    # newAtt={"ExID":att[1] , "StuID":stuid , "TchID":tchid , "score":0 , "duration_S":0}
+    st.write(score)
+else :
+    st.title(qustions[index][2])
+
+    choices = db.get("choices", {"QusID":qustions[index][0]})
+    for choice in choices:
+        choiceHeader=choice[2]
+        if st.button(choiceHeader):
+            st.session_state.score =score+choice[3]
+            st.session_state.index = index+1
+            st.rerun()
+       
