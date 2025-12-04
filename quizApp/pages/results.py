cdea7f721c2ff2efd1635430007c967a5987244c
mennanaam6-code <mennanaam6@gmail.com>
2025-12-04 14:56:10 +0200

    .

--- quizApp/pages/results.py
+++ quizApp/pages/results.py
@@ -12,13 +12,15 @@ import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..")))
 import streamlit as st
 from quizApp.dataBase import init_db
+import pandas as pd
 db = init_db.dbIns
 
 query = """
-select e.title,u.userName, a.score from attempts a
+select e.title,u.userName, a.score,a.numQus,a.duration_S,a.total_time,a.start_at from attempts a
 join exams e on e.ExID = a.Exid
 join users u on u.Uid = a.Tchid
 """
 params = ()
 result =db.query(query,params)
-st.write(result)
+df=pd.DataFrame(result,columns=[ 'Exam Title','teacher name','Score','numQus','Duration','total time','Attempt Date'])
+st.dataframe(df)
